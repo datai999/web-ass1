b9291750f7b829dbd02a95a13ec10739d73a4bae
Nguyen Duc Anh Tai <anhtai.nguyenduc@citynow.vn>
2020-11-22 10:15:38 +0700

    remove file

--- learn_css/script.js
+++
@@ -1,9 +0,0 @@
-/* Open */
-function openNav() {
-    document.getElementsByClassName(active_navbar).style.height = "147px";
-}
-  
-  /* Close */
-function closeNav() {
-    document.getElementsByClassName(active_navbar).style.height = "0px";
-}
